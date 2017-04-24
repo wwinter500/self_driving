3c03173933c7269c77bb98bdba799cc8b2f5c60d
wwinter500 <ustchope500@gmail.com>
2017-04-24 16:11:24 -0700

    "project5"

---
+++ submit/pro5/box.py
@@ -0,0 +1,6 @@
+class Box:
+    def __init__(self):
+        self.x, self.y = float(), float()
+        self.w, self.h = float(), float()
+        self.c = float()
+        self.prob = float()
---
+++ submit/pro5/train.py
@@ -0,0 +1,66 @@
+import keras
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Flatten, Dense, Activation, Reshape
+from keras.layers.advanced_activations import LeakyReLU
+
+from keras.models import Sequential
+
+import numpy as np
+#from utils import load_weights
+
+##build tiny yolo model by using COCO dataweights datasets
+
+keras.backend.set_image_dim_ordering('th')
+
+model = Sequential()
+model.add(Convolution2D(16, 3, 3,input_shape=(3,448,448),border_mode='same',subsample=(1,1)))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(32,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),border_mode='valid'))
+model.add(Convolution2D(64,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),border_mode='valid'))
+model.add(Convolution2D(128,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),border_mode='valid'))
+model.add(Convolution2D(256,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),border_mode='valid'))
+model.add(Convolution2D(512,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),border_mode='valid'))
+model.add(Convolution2D(1024,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(Convolution2D(1024,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(Convolution2D(1024,3,3 ,border_mode='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Dense(4096))
+model.add(LeakyReLU(alpha=0.1))
+model.add(Dense(1470))
+
+
+#load weights of COCO datasets
+def load_weights(model,weight_file):
+                
+    data = np.fromfile(weight_file,np.float32)
+    data=data[4:]
+    
+    index = 0
+    for layer in model.layers:
+        shape = [w.shape for w in layer.get_weights()]
+        if shape != []:
+            kshape,bshape = shape
+            bia = data[index:index+np.prod(bshape)].reshape(bshape)
+            index += np.prod(bshape)
+            ker = data[index:index+np.prod(kshape)].reshape(kshape)
+            index += np.prod(kshape)
+            layer.set_weights([ker,bia])
+
+
+load_weights(model,'/Users/user/Downloads/Self_Driving_Doc/yolov1.weights')
+
